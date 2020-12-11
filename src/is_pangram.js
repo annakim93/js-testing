17d4a2504743830a3535d948bd8fdb8e38d75afd
Anna Kim <anna.kim93@gmail.com>
2020-12-10 19:36:12 -0500

    completed is_pangram and passes all tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,24 @@
 const isPangram = function(text) {
-  return true
+  if (text.length < 26) {
+    return false;
+  }
+
+  const lowercaseText = text.toLowerCase();
+  
+  let alphaChars = {};
+  for (let i = 0; i < lowercaseText.length; i++) {
+    if (lowercaseText.charCodeAt(i) > 96 && lowercaseText.charCodeAt(i) < 123) {
+      if (!alphaChars[lowercaseText[i]]) {
+        alphaChars[lowercaseText[i]] = 1;
+      }
+    }
+  }
+
+  if (Object.keys(alphaChars).length == 26) {
+    return true;
+  }
+
+  return false;
 };
 
 module.exports = isPangram;
